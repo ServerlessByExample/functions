1a5b55f066a3f245e848fa2453aab86622635468
aswinkarthik <aswin.k@aux.go-jek.com>
2017-12-16 07:20:41 +0530

    Running tests for node and go

--- brigade.js
+++ brigade.js
@@ -1,4 +1,42 @@
-const { events } = require('brigadier');
+const { events, Job, Group } = require('brigadier');
+
+const createJob = ({id, name, image, tasks}) => {
+  let job = new Job(`${name}-${id}`, image);
+  job.tasks = tasks;
+  return job;
+};
+
+const run = async () => {
+  let group = new Group();
+  group.add(createJob(
+    {
+      id: 1,
+      name: 'test-go',
+      image: 'golang:1.8-jessie',
+      tasks: [
+        'cd /src/go',
+        'go test',
+      ]
+    },
+    {
+      id: 2,
+      name: 'test-node',
+      image: 'node:latest',
+      tasks: [
+        'cd /src/node',
+        'yarn install',
+        'yarn test',
+      ],
+    }
+  ));
+
+  await group.runAll();
+};
+
+events.on("exec", async (brigadeEvent, project) => {
+  run();
+  console.log("Done running")
+});
 
 events.on('push', (brigadeEvent, project) => {
   console.log('Hello world!');
